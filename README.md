d1d3f03cc0a09c5aad434488846198b7d032b172
Christophe Grand <christophe@cgrand.net>
2017-09-21 16:17:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# nrepl-bridge
+
+A nrepl client which recreates a traditional repl streams pair.
+
+## License
+
+Copyright © 2017 Christophe Grand
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject unrepl/nrepl-bridge "0.1.0-SNAPSHOT"
+  :description "A bridge to recreate a plain repl on top of a nrepl connection."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.nrepl "0.2.12"]])
